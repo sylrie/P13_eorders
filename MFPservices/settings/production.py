01641e27e3f4a74dbfc6893587829d21085cef1c
Sylvain <sylvain.rieutor@orange.fr>
2020-08-20 15:00:59 +0200

    requirements

--- MFPservices/settings/production.py
+++ MFPservices/settings/production.py
@@ -1,8 +1,5 @@
 from . import *
 import os
-import raven
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
