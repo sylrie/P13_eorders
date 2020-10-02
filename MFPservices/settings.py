339505182afb77dcb74559cfda137f2118830d71
Sylvain <sylvain.rieutor@orange.fr>
2020-10-02 09:16:13 +0200

    modif ux table select

--- MFPservices/settings.py
+++ MFPservices/settings.py
@@ -73,7 +73,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=command, product, user, message',
+    '--cover-package=command, product',
 ]
 
 SITE_ID = 1
